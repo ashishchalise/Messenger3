cfe70944474f35833e990b0b042ddfb6dcb0305a
atulbh <atul.krbhatia@gmail.com>
2017-02-02 09:24:55 +0530

    Update MessageService.java

--- src/main/java/org/koushik/javabrains/messenger/service/MessageService.java
+++ src/main/java/org/koushik/javabrains/messenger/service/MessageService.java
@@ -14,8 +14,8 @@ public class MessageService {
 	private Map<Long, Message> messages = DatabaseClass.getMessages();
 	
 	public MessageService() {
-		messages.put(1L, new Message(1, "Hello World this is me", "Priyanka Bhatia"));
-		messages.put(2L, new Message(2, "Hello Jersey this is me", "Atul Bhatia"));
+		messages.put(1L, new Message(1, "Hello guys this is done by automation", "New Author"));
+		messages.put(2L, new Message(2, "Hello Jersey this is me", "Team Lead"));
 	}
 	
 	public List<Message> getAllMessages() {
